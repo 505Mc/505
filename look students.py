62d24a79f3abe0144cc39e26f353d529b0120c05
505Mc <2128361914@qq.com>
2026-06-09 16:37:59 +0800

    age

--- look students.py
+++
@@ -1,6 +0,0 @@
-students=["Alice","Bob","Charlie","David","Eve"]
-name=input("students:")
-print("student:",name,students.index(name))
-                               
-
-
--- read_conflict_file.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-one_folder = Path.home() / "Desktop" / "one"
-file_path = one_folder / "Untitled-1.py"
-content = file_path.read_bytes()
-print(content.decode("utf-8", errors="replace"))
