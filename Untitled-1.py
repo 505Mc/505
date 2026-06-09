816bc28028fd7db965978f3af29d63a634bc22dc
505Mc <2128361914@qq.com>
2026-06-09 11:41:59 +0800

    test direct push

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,9 +1,13 @@
-print("你好世界")
+print("欢迎来到年龄计算器！")
 age = 6
 add = int(input("年龄增长："))
 final_age = age + add
+
 print("你的最终年龄：", final_age)
+
 if final_age >= 18:
     print("你成年了")
 else:
     print("你未成年")
+
+    年龄增长:12
