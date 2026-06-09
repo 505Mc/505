555dd908b49c44e3aee37b497cc31a5f3998aacd
505Mc <2128361914@qq.com>
2026-06-09 12:16:18 +0800

    你好世界

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,9 +1,16 @@
 print("你好世界")
 age = 6
-add = int(input("年龄增长："))
-final_age = age + add
-print("你的最终年龄：", final_age)
-if final_age >= 18:
-    print("你成年了")
-else:
-    print("你未成年")
+
+# 这是防护代码，输入字母/符号都不会崩溃
+try:
+    add = int(input("年龄增长："))
+    final_age = age + add
+    print("你的最终年龄：", final_age)
+    
+    if final_age >= 18:
+        print("你成年了")
+    else:
+        print("你未成年")
+
+except ValueError:
+    print("输入有误！请输入数字")
